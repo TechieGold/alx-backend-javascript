e7812a42a94f15d672aba0d760473d86589915f1
Gold Israel <techiegold101@gmail.com>
2024-03-08 10:38:55 +0100

    function that create new instance of a class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+// 0-classroom.js - Represents a classrom with a maximum capacity of students.
+
+export default class ClassRoom {
+  /**
+     * Creates an instance of ClassRoom.
+     * @param {number} maxStudentsSize  - The maximum capacity of the classroom.
+     */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
