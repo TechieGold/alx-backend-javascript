657ff0cedcd2219664d7d052a653db6f6bb4dbf7
Gold Israel <techiegold101@gmail.com>
2024-03-11 16:43:19 +0100

    add function that returns array list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// This function returns an array of list.
+
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
