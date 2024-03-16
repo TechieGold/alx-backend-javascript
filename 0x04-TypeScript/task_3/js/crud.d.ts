44c0a53df09b51d354080bfca17522dc0db9e237
Gold Israel <techiegold101@gmail.com>
2024-03-17 00:03:26 +0100

    dependecy script

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+export {RowId, RowElement} from './interface';
+
+declare function insertRow(row: RowElement): RowID;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
